78863d5e80f114df3c241b710f5433e3a3fb504d
MaxLe2901 <55411378+MaxLe2901@users.noreply.github.com>
2025-03-07 10:52:23 -0500

    Commit and push

---
+++ Test Suites/Healthcare_Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Healthcare_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>405ce7eb-66a6-41f2-872b-56dc53d535e4</testSuiteGuid>
+</TestSuiteEntity>
