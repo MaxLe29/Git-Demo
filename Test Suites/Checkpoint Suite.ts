ce6b5240a47cb30760b1a097b0710b0378ff66ff
MaxLe2901 <55411378+MaxLe2901@users.noreply.github.com>
2025-03-19 16:12:54 -0400

    Commit

---
+++ Test Suites/Checkpoint Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Checkpoint Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ea398d15-c75a-484a-be5d-28cbdabb7a52</testSuiteGuid>
+   <testCaseLink>
+      <guid>ad3e6a33-005d-4f46-8605-c6f41ebb688c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkpoint Test/Checkpoint</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
